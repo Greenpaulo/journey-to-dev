3e16373cddd9e73cec34754214b7a4e6d65817d6
Paul Bingham <greenpaulo@yahoo.co.uk>
2020-07-02 15:10:12 +0100

    Added favicon

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -4,6 +4,7 @@
       <meta charset="utf-8">
       <meta name="csrf-token" content="{{csrf_token()}}">
       <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="icon" href="/images/favicon.ico">
       <link rel="stylesheet" href="/css/app.css">
       <!-- Bootswatch -->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/cyborg/bootstrap.min.css">
